c409713a333aec3ad87bd0557c46ca408609c556
jmediamanager <werndlymob@gmail.com>
2011-11-28 02:04:42 +0000

    git-svn-id: http://jmm.googlecode.com/svn/trunk@303 a6988de2-25aa-124f-8851-278695053f3e

---
+++ JMMClient/AppStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.IO;
+
+namespace JMMClient
+{
+	public class AppStartup
+	{
+		[STAThread]
+		static public void Main(string[] args)
+		{
+			try
+			{
+				App app = new App();
+				app.InitializeComponent();
+				app.Run();
+			}
+			catch (Exception ex)
+			{
+				File.WriteAllText(@"C:\jmmerror.txt", ex.ToString());
+				MessageBox.Show(ex.Message + "\r\r" + ex.StackTrace, "Application Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+	}
+}
