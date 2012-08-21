694b93634c383dff724f81365e747d4b7e188f6b
jmediamanager <werndlymob@gmail.com>
2012-08-21 06:55:48 +0000

    Fix handling of torrent search results from Nyaa, when only one result is returned
    git-svn-id: http://jmm.googlecode.com/svn/trunk@954 a6988de2-25aa-124f-8851-278695053f3e

--- JMMClient/Downloads/TorrentsNyaa.cs
+++ JMMClient/Downloads/TorrentsNyaa.cs
@@ -148,6 +148,101 @@ namespace JMMClient.Downloads
 			return torLinks;
 		}
 
+		private List<TorrentLinkVM> ParseSourceSingleResult(string output)
+		{
+			List<TorrentLinkVM> torLinks = new List<TorrentLinkVM>();
+
+			char q = (char)34;
+			string quote = q.ToString();
+
+			// Name:</td><td class="tinfotorrentname">
+			string startBlock = @"Name:";
+
+			// class="thead">Name:</td><td class="tinfotorrentname">[Hadena] Koi to Senkyo to Chocolate - 03 [720p] [9CD64623].mkv</td>
+			string nameStart = "tinfotorrentname" + quote + ">";
+			string nameEnd = "</td>";
+
+			// Seeders:</td><td class="vtop"><span class="tinfosn">17</span>
+			string seedStart = "Seeders:</td><td class=" + quote + "vtop" + quote + "><span class=" + quote + "tinfosn" + quote + ">";
+			string seedEnd = "</span>";
+
+			// Leechers:</td><td class="vtop"><span class="tinfoln">
+			string leechStart = "Leechers:</td><td class=" + quote + "vtop" + quote + "><span class=" + quote + "tinfoln" + quote + ">";
+			string leechEnd = "</span>";
+
+			// File size:</td><td class="vtop">193.8 MiB</td>
+			string sizeStart = "File size:</td><td class=" + quote + "vtop" + quote + ">";
+			string sizeEnd = "</td>";
+
+			// class="tinfodownloadbutton"><a href="http://www.nyaa.eu/?page=download&#38;tid=334194"
+			string torStart = "class=" + quote + "tinfodownloadbutton" + quote + "><a href=" + quote;
+			string torEnd = quote;
+
+
+			int pos = output.IndexOf(startBlock, 0);
+			while (pos > 0)
+			{
+
+				if (pos <= 0) break;
+
+				int posNameStart = output.IndexOf(nameStart, pos + 1);
+				int posNameEnd = output.IndexOf(nameEnd, posNameStart + nameStart.Length + 1);
+
+				string torName = output.Substring(posNameStart + nameStart.Length, posNameEnd - posNameStart - nameStart.Length);
+
+				string torSeed = "";
+				int posSeedStart = output.IndexOf(seedStart, posNameEnd);
+				int posSeedEnd = 0;
+				if (posSeedStart > 0)
+				{
+					posSeedEnd = output.IndexOf(seedEnd, posSeedStart + seedStart.Length + 1);
+					torSeed = output.Substring(posSeedStart + seedStart.Length, posSeedEnd - posSeedStart - seedStart.Length);
+				}
+
+				string torLeech = "";
+				int posLeechStart = output.IndexOf(leechStart, posSeedEnd + 3);
+				int posLeechEnd = 0;
+				if (posLeechStart > 0)
+				{
+					posLeechEnd = output.IndexOf(leechEnd, posLeechStart + leechStart.Length + 1);
+					torLeech = output.Substring(posLeechStart + leechStart.Length, posLeechEnd - posLeechStart - leechStart.Length);
+				}
+
+				string torSize = "";
+				int posSizeStart = output.IndexOf(sizeStart, posLeechEnd);
+				int posSizeEnd = 0;
+				if (posSizeStart > 0)
+				{
+					posSizeEnd = output.IndexOf(sizeEnd, posSizeStart + sizeStart.Length + 1);
+					torSize = output.Substring(posSizeStart + sizeStart.Length, posSizeEnd - posSizeStart - sizeStart.Length);
+				}
+
+
+				int posTorStart = output.IndexOf(torStart, posSizeEnd);
+				int posTorEnd = output.IndexOf(torEnd, posTorStart + torStart.Length + 1);
+
+				string torLink = output.Substring(posTorStart + torStart.Length, posTorEnd - posTorStart - torStart.Length);
+				torLink = DownloadHelper.FixNyaaTorrentLink(torLink);
+
+				// remove html codes
+				torLink = HttpUtility.HtmlDecode(torLink);
+
+				TorrentLinkVM torrentLink = new TorrentLinkVM(SourceType);
+				torrentLink.TorrentDownloadLink = torLink;
+				torrentLink.TorrentName = torName;
+				torrentLink.Size = torSize.Trim();
+				torrentLink.Seeders = torSeed.Trim();
+				torrentLink.Leechers = torLeech.Trim();
+				torLinks.Add(torrentLink);
+
+				pos = output.IndexOf(startBlock, pos + 1);
+
+			}
+			//Console.ReadLine();
+
+			return torLinks;
+		}
+
 		public List<TorrentLinkVM> GetTorrents(List<string> searchParms)
 		{
 			string urlBase = "http://www.nyaa.eu/?page=search&cats=1_37&filter=0&term={0}";
@@ -163,9 +258,12 @@ namespace JMMClient.Downloads
 			string output = Utils.DownloadWebPage(url);
 
 			logger.Trace("GetTorrents Search: " + url);
-			//logger.Trace("GetTorrents Results: " + output);
 
-			return ParseSource(output);
+			// on nyaa if you search and there is only one result it goes straight to the dedicated torrent page
+			if (output.Contains("Searching torrents"))
+				return ParseSource(output);
+			else
+				return ParseSourceSingleResult(output);
 		}
 
 		public List<TorrentLinkVM> BrowseTorrents()
