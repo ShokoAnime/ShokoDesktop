4d8ffcc3fdacf51fc1b655166817c53faa79270c
jmediamanager <werndlymob@gmail.com>
2012-11-09 03:03:58 +0000

    More fixes to normal dashboard
    git-svn-id: http://jmm.googlecode.com/svn/trunk@1022 a6988de2-25aa-124f-8851-278695053f3e

---
+++ JMMClient/UserControls/CustomToolTip.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JMMClient
+{
+   public class CustomToolTip : ToolTip
+   {
+      public static readonly DependencyProperty HasRoundCornersProperty =
+         DependencyProperty.Register("HasRoundCorners", typeof(bool), typeof(CustomToolTip), new FrameworkPropertyMetadata(true));
+
+      static CustomToolTip()
+      {
+         DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomToolTip), new FrameworkPropertyMetadata(typeof(CustomToolTip)));
+      }
+
+      public bool HasRoundCorners
+      {
+         get { return (bool)GetValue(HasRoundCornersProperty); }
+         set { SetValue(HasRoundCornersProperty, value); }
+      }
+   }
+}
---
+++ JMMClient/UserControls/DashboardEpisodeOverview.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JMMClient.UserControls
+{
+   /// <summary>
+   /// Interaction logic for DashboardEpisodeOverview.xaml
+   /// </summary>
+   public partial class DashboardEpisodeOverview : UserControl
+   {
+      public DashboardEpisodeOverview()
+      {
+         InitializeComponent();
+      }
+
+      private void ImageBorder_Loaded(object sender, RoutedEventArgs e)
+		{
+			Border border = (Border)sender;
+			Thickness borderThickness = border.BorderThickness;
+
+			if (border.ActualWidth <= borderThickness.Left + borderThickness.Right ||
+				border.ActualHeight <= borderThickness.Top + borderThickness.Bottom)
+			{ // When the border contains no content, hide the border so that an empty border isn't rendered
+				border.Visibility = Visibility.Hidden;
+			}
+		}
+	}
+}
