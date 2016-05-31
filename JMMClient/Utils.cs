d880a1c985c825aebe01454403036daa1cab300a
Craige1 <craigemmott@hotmail.com>
2016-06-01 01:00:25 +0930

    Check to see if JMMServer is already running locally before trying to start it locally.

--- JMMClient/Utils.cs
+++ JMMClient/Utils.cs
@@ -158,6 +158,9 @@ namespace JMMClient
                 return false;
             }
 
+            Process[] pname = Process.GetProcessesByName("JMMServer.exe");
+            if (pname.Length == 0) return true;
+
             try
             {
                 logger.Info("Starting JMM Server");
