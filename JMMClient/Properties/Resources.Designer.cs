4afce313a744a7050c716f13d5507c607a77384e
Timofej Dod <hidden@4you.lt>
2016-07-23 21:07:33 +0300

    New server status for LinkAniDBTvDB

--- JMMClient/Properties/Resources.Designer.cs
+++ JMMClient/Properties/Resources.Designer.cs
@@ -1609,6 +1609,15 @@ namespace JMMClient.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Updating Changed TvDB association: {0}.
+        /// </summary>
+        public static string Command_LinkAniDBTvDB {
+            get {
+                return ResourceManager.GetString("Command_LinkAniDBTvDB", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Paused.
         /// </summary>
         public static string Command_Paused {
