73bbae326a562ea14407322159468bca5181d682
da3dsoul <da3dsoul@gmail.com>
2017-06-27 15:15:40 -0400

    Defrag LOH on refresh

--- Shoko.Desktop/ViewModel/VM_MainListHelper.cs
+++ Shoko.Desktop/ViewModel/VM_MainListHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
@@ -790,6 +791,7 @@ namespace Shoko.Desktop.ViewModel
                 }
                 LastChange = changes.LastChange;
 
+                GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
                 GC.Collect();
             }
             catch (Exception ex)
@@ -825,6 +827,8 @@ namespace Shoko.Desktop.ViewModel
             {
                 v.PopulateSerieInfo(AllGroupsDictionary, AllSeriesDictionary);
             }
+
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
             GC.Collect();
         }
 
