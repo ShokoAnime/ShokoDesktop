564301ab58dfce9301b0a93fba4e69d94d18642f
da3dsoul <da3dsoul@gmail.com>
2017-09-04 16:16:43 -0400

    Remove unused file. Make Nep Nep show in more cases where there should be Nep Nep. Also log them

--- Shoko.Desktop/ViewModel/Server/VM_AniDB_Anime.cs
+++ Shoko.Desktop/ViewModel/Server/VM_AniDB_Anime.cs
@@ -278,10 +278,7 @@ namespace Shoko.Desktop.ViewModel.Server
                 {
                     ImageDownloadRequest req = new ImageDownloadRequest(ImageEntityType.AniDB_Cover, this, false);
                     MainWindow.imageHelper.DownloadImage(req);
-                    if (File.Exists(fileName)) return fileName;
-
-                    string packUriBlank = $"pack://application:,,,/{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name};component/Images/blankposter.png";
-                    return packUriBlank;
+                    return fileName;
                 }
                 return fileName;
             }
@@ -708,7 +705,7 @@ namespace Shoko.Desktop.ViewModel.Server
         {
             get
             {
-                string packUriBlank = $"pack://application:,,,/{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name};component/Images/blankposter.png";
+                string packUriBlank = $"pack://application:,,,/{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name};component/Images/blankfanart.png";
 
                 // this should be randomised or use the default 
                 if (DefaultImageFanart != null)
