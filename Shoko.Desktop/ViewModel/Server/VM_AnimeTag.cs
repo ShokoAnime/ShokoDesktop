e7bacb3e0b85879d0ebeb025249ddb5e976c388f
da3dsoul <da3dsoul@gmail.com>
2017-09-18 13:43:16 -0400

    Tag interning happens deeper now, so only handle INotifyPropertyChangedExt

--- Shoko.Desktop/ViewModel/Server/VM_AnimeTag.cs
+++ Shoko.Desktop/ViewModel/Server/VM_AnimeTag.cs
@@ -14,14 +14,14 @@ namespace Shoko.Desktop.ViewModel.Server
 
         public new string TagName
         {
-            get => base.TagName == null ? null : string.Intern(base.TagName);
-            set => this.SetField(() => base.TagName, (r) => base.TagName = r == null ? null : string.Intern(r), value);
+            get => base.TagName;
+            set => this.SetField(() => base.TagName, (r) => base.TagName = r, value);
         }
 
         public new string TagDescription
         {
-            get => base.TagDescription == null ? null : string.Intern(base.TagDescription);
-            set => this.SetField(() => base.TagDescription, (r) => base.TagDescription = r == null ? null : string.Intern(r), value);
+            get => base.TagDescription;
+            set => this.SetField(() => base.TagDescription, (r) => base.TagDescription = r, value);
         }
     }
 }
