a268052067c209a604af42a3b0b06aabf9212d63
Unknown <richard@richardvanhorn.com>
2018-06-21 21:13:32 -0700

    Update Installer.

--- Shoko.Desktop/Properties/AssemblyInfo.cs
+++ Shoko.Desktop/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.8.3.0")]
-[assembly: AssemblyFileVersion("3.8.3.0")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
