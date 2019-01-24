33bd38776c3c506dd3a71eceb39fb215c06de0cd
Baine <Baine@users.noreply.github.com>
2019-01-24 17:29:45 +0100

    [F] Fix sorting by Size
    [*] make sorting by Size obey the actual number based sorting
    [*] also fix size suffixes (converter provided by @da3dsoul )
    
    Co-Authored-By: da3dsoul <da3dsoul@gmail.com>

--- Shoko.Desktop/WPFHelpers/LongToFileSizeConverter.cs
+++ Shoko.Desktop/WPFHelpers/LongToFileSizeConverter.cs
@@ -27,11 +27,11 @@ namespace Shoko.Desktop.WPFHelpers
         {
             string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
             if (byteCount == 0)
-                return "0" + suf[0];
+                return "0" + " " + suf[0];
             long bytes = Math.Abs(byteCount);
             int place = System.Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
             double num = Math.Round(bytes / Math.Pow(1024, place), 2);
-            return num + suf[place];
+            return num + " " + suf[place];
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
