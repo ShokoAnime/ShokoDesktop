9b6802141125bc8ee3b0890f5a99acb094a9906e
da3dsoul <da3dsoul@gmail.com>
2020-06-08 14:45:16 -0400

    Fix Streaming

--- Shoko.Desktop/VideoPlayers/Extensions.cs
+++ Shoko.Desktop/VideoPlayers/Extensions.cs
@@ -22,7 +22,7 @@ namespace Shoko.Desktop.VideoPlayers
             {
                 if (vid.Media?.Parts == null || vid.Media.Parts.Count == 0)
                     throw new Exception("There is no media information loaded in the video selected, we're unable to stream the media");
-                Tuple<string, List<string>> t = GetInfoFromMedia(vid.Media);
+                Tuple<string, List<string>> t = GetInfo(vid.VideoLocalID, vid.FullPath, vid.Media);
                 return new VideoInfo
                 {
                     Uri = t.Item1,
@@ -52,7 +52,7 @@ namespace Shoko.Desktop.VideoPlayers
             {
                 if (vid.Media?.Parts == null || vid.Media.Parts.Count == 0)
                     throw new Exception("There is no media information loaded in the video selected, we're unable to stream the media");
-                Tuple<string, List<string>> t = GetInfoFromMedia(vid.Media);
+                Tuple<string, List<string>> t = GetInfo(vid.VideoLocalID, vid.FullPath, vid.Media);
                 return new VideoInfo
                 {
                     Uri = t.Item1,
@@ -77,51 +77,38 @@ namespace Shoko.Desktop.VideoPlayers
             };
         }
 
-        static Tuple<string, List<string>> GetInfoFromMedia(Media m)
+        static Tuple<string, List<string>> GetInfo(int vlID, string path, Media m)
         {
-            WebClient wc = new WebClient();
             List<string> subs = new List<string>();
             //Only Support one part for now
-
-            Part p = m.Parts[0];
-            string fullname = p.Key.Replace("\\", "/").Replace("//", "/").Replace(":", string.Empty);
-            string uri = $"http://{AppSettings.JMMServer_Address}:{AppSettings.JMMServer_Port}" +
-                $"{new Uri(p.Key).GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped)}";
-            string fname = Path.GetFileNameWithoutExtension(fullname);
-            if (p.Streams != null)
+            string uri =
+                $"http://{AppSettings.JMMServer_Address}:{AppSettings.JMMServer_Port}/Stream/{vlID}/{VM_ShokoServer.Instance.CurrentUser.JMMUserID}/false/null";
+            string fname = Path.GetFileNameWithoutExtension(path);
+            var p = m?.Parts?.FirstOrDefault();
+            if (p?.Streams == null) return new Tuple<string, List<string>>(uri, subs);
+            WebClient wc = new WebClient();
+            foreach (Models.PlexAndKodi.Stream s in p.Streams.Where(a => a.File != null && a.StreamType == 3))
             {
-                foreach (Models.PlexAndKodi.Stream s in p.Streams.Where(a => a.File != null && a.StreamType == 3))
+                string extension = Path.GetExtension(s.File);
+                string filePath = Path.Combine(Path.GetTempPath(), Path.GetFileName(Path.GetDirectoryName(path)));
+                try
                 {
-                    string extension = Path.GetExtension(s.File);
-                    string filePath = Path.Combine(Path.GetTempPath(), Path.GetDirectoryName(fullname));
+                    string subtitle = wc.DownloadString(s.Key);
                     try
                     {
-                        string subtitle = wc.DownloadString(s.Key);
-                        /*   try
-                           {
-                               Directory.CreateDirectory(filePath);
-                               string fullpath = Path.Combine(filePath, fname + extension);
-                               File.WriteAllText(filePath, subtitle);
-                           }
-                           catch (Exception)
-                           {
-                           }*/
-                        try
-                        {
-                            filePath = Path.Combine(Path.GetTempPath(), fname + extension);
-                            File.WriteAllText(filePath, subtitle);
-                            subs.Add(filePath);
-                        }
-                        catch (Exception ex)
-                        {
-                            logger.Warn("Cannot download subtitle: " + ex);
-                        }
-
+                        filePath = Path.Combine(Path.GetTempPath(), fname + extension);
+                        File.WriteAllText(filePath, subtitle);
+                        subs.Add(filePath);
                     }
-                    catch (Exception e)
+                    catch (Exception ex)
                     {
-                        logger.Warn("Cannot download subtitle: " + e);
+                        logger.Warn("Cannot download subtitle: " + ex);
                     }
+
+                }
+                catch (Exception e)
+                {
+                    logger.Warn("Cannot download subtitle: " + e);
                 }
             }
             return new Tuple<string, List<string>>(uri, subs);
