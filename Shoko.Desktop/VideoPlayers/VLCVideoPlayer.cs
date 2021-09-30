0f9e3909fda89a22104b6f93326ae66f30481cc4
Rawrskyes <rawrskyes@gmail.com>
2021-09-30 19:05:33 +1000

    Fixed playback detection

--- Shoko.Desktop/VideoPlayers/VLCVideoPlayer.cs
+++ Shoko.Desktop/VideoPlayers/VLCVideoPlayer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -231,7 +231,7 @@ namespace Shoko.Desktop.VideoPlayers
                         {
                             // Extract filename and url decode so it matches
                             filename = HttpUtility.UrlDecode(fileRegex.Match(responseString).Groups[1].ToString());
-                            filename = filename.Replace("file:", string.Empty).Replace("/", "\\").Trim();
+                            filename = filename.Replace("file:///", string.Empty).Replace("/", "\\").Trim();
                         }
                     }
                 }
