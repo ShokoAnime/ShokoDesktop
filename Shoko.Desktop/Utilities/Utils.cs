0ae8a0d8c831ec6de771af602158d7a92a264159
Baine <Baine@users.noreply.github.com>
2022-11-14 11:49:29 +0100

    fix OpenURL
    fix handling of ampersand

--- Shoko.Desktop/Utilities/Utils.cs
+++ Shoko.Desktop/Utilities/Utils.cs
@@ -531,7 +531,6 @@ namespace Shoko.Desktop.Utilities
                 // hack because of this: https://github.com/dotnet/corefx/issues/10361
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    url = url.Replace("&", "^&");
                     Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
