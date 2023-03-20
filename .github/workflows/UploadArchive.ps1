39185dac42283b58cd093bdc529e56a054160408
da3dsoul <da3dsoul@gmail.com>
2023-03-20 16:02:10 -0400

    Hopefully fix upload

--- .github/workflows/UploadArchive.ps1
+++ .github/workflows/UploadArchive.ps1
@@ -4,4 +4,4 @@ $ftp_server = $env:FTP_SERVER;
 $current = [string](Get-Location);
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($user, $password);
-$client.UploadFile([string]($ftp_server) + "/shoko-desktop/daily/ShokoDesktop.zip", [string]($current) + "\\ShokoDesktop.zip");
+$client.UploadFile([string]($ftp_server) + "/files/shoko-desktop/daily/ShokoDesktop.zip", [string]($current) + "\\ShokoDesktop.zip");
