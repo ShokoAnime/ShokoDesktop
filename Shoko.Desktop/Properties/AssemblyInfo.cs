d8b85256284ce5da126310c9c80f688af2940ee8
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-03-20 17:51:54 -0700

    Update Installed and Version

--- Shoko.Desktop/Properties/AssemblyInfo.cs
+++ Shoko.Desktop/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.2.0")]
-[assembly: AssemblyFileVersion("4.1.2.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
